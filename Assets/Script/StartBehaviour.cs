ff7aa027cf6138889c4dc87ef57447d0cd638967
Ivo Henriques <ijsh2000@outlook.com>
2025-09-11 15:52:19 +0100

    Room walls

---
+++ Assets/Script/StartBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using XRoam.Core.StateMachine;
+using XRoam.Services.StateMachine;
+
+public class StartBehaviour : MonoBehaviour
+{
+    [SerializeField] private State _state;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StateMachineService.Instance.ChangeState(_state.Id, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
